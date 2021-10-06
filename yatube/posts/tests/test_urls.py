678dc5ec7de98453e5310fab8bc4dfdba4a07f5c
vovanbart <vovanbart@icloud.com>
2021-10-07 00:07:17 +0300

    5

--- yatube/posts/tests/test_urls.py
+++ yatube/posts/tests/test_urls.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase, Client
-from http import HTTPStatus
 
 from posts.models import Post, Group
 
@@ -29,30 +28,6 @@ class PostURLTests(TestCase):
         self.user = PostURLTests.user
         self.authorized_client = Client()
         self.authorized_client.force_login(self.user)
-
-    def test_url_exists_at_desired_location(self):
-        url = [
-            '/',
-            '/group/test_slug/',
-            '/profile/auth/',
-            '/posts/1/',
-            '/create/',
-            '/posts/1/edit/',
-        ]
-        for adress in url:
-            authorized_adress = ['/create/', '/posts/1/edit/']
-            with self.subTest(adress=adress):
-                response = self.guest_client.get(adress)
-                if adress in authorized_adress:
-                    self.assertEqual(
-                        response.status_code,
-                        HTTPStatus.FOUND,
-                        f'Адресс {adress} не доступен.')
-                else:
-                    self.assertEqual(
-                        response.status_code,
-                        HTTPStatus.OK,
-                        f'Адресс {adress} не доступен.')
 
     def test_edit_author_post(self):
         author = PostURLTests.post.author
